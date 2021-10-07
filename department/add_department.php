577d6c8f3a594eddc7e6abd9ac77c1060679df5e
AhmedArafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-10-07 17:10:24 +0200

    Update add_department.php

--- department/add_department.php
+++ department/add_department.php
@@ -1,5 +1,5 @@
 <?php
-include '/xampp/htdocs/HRSystem/ConfigDB.php';
+include '/opt/lampp/htdocs/HR-System-Project/ConfigDB.php';
 
 //If User click on Submit Buttom after entering data of employee
 if(isset($_POST['send'])){
@@ -34,7 +34,7 @@ if(isset($_GET['edit'])){
 }
 
 if($_SESSION['admin']){} 
-else header('location:/HRSystem/Admins/Login.php');
+else header('location:/opt/lampp/htdocs/HR-System-Project/Admins/Login.php');
 
 ?>
 <!-- HTML CODE -->
@@ -49,7 +49,7 @@ else header('location:/HRSystem/Admins/Login.php');
 
 <body>
 <?php
-include '/xampp/htdocs/HRSystem/Nav.php';
+include '/opt/lampp/htdocs/HR-System-Project/Nav.php';
 ?>
     <div class="container col-md-6 mt-5">
         <form method="POST" enctype="multipart/form-data">
